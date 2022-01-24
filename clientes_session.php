cfd4efe39968c6099b9ea6b88618f8fb667aeb73
leandroamar <leandro.mathias.amar@gmail.com>
2022-01-24 18:24:12 -0300

    cambios

--- clientes_session.php
+++ clientes_session.php
@@ -2,28 +2,33 @@
 
 
 session_start();
-if (!isset($_SESSION["array"])) {
-    $_SESSION["array"] = array();
+if (!isset($_SESSION["listadoClientes"])) {
+    $_SESSION["listadoClientes"] = array();
 }
 
 //session_destroy();
 
 
 if ($_POST) {
+    $nombre = $_REQUEST["txtNombre"];
+    $dni = $_REQUEST["txtDni"];
+    $telefono = $_REQUEST["txtTelefono"];
+    $edad = $_REQUEST["txtEdad"];
 
 
     if (isset($_POST["btnAgregar"])) {
-        $_SESSION["array"][] = array(
-            "nombre" => $_REQUEST["txtNombre"],
-            "dni"  => $_REQUEST["txtDni"],
-            "telefono"  => $_REQUEST["txtTelefono"],
-            "edad"  => $_REQUEST["txtEdad"]
+        
+        $_SESSION["listadoClientes"][] = array(
+            "nombre" => $nombre,
+            "dni"  => $dni,
+            "telefono"  => $telefono,
+            "edad"  => $edad
         );
     }
     if (isset($_POST["btnEliminar"])) {
         
         session_destroy();
-        $_SESSION["array"] = array();
+        $_SESSION["listadoClientes"] = array();
     }
 }
 ?>
@@ -35,7 +40,7 @@ if ($_POST) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Listado de clientes</title>
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
@@ -44,7 +49,7 @@ if ($_POST) {
     <div class="container">
         <div class="row">
             <div class="col-12 text-center py-5">
-                <h1> Tabla de clientes </h1>
+                <h1> Listado de clientes </h1>
             </div>
         </div>
         <div class="row">
@@ -63,7 +68,7 @@ if ($_POST) {
                     <input type="text" name="txtEdad" class="form-control my-2" placeholder="99">
 
                     <input type="submit" name="btnAgregar" class="btn bg-primary text-white" value="Enviar">
-                    <input type="submit" name="btnEliminar" class="btn bg-danger text-white" value="eliminar">
+                    <input type="submit" name="btnEliminar" class="btn bg-danger text-white" value="Eliminar">
                 </form>
             </div>
             <div class="col-6 ms-5">
@@ -76,7 +81,7 @@ if ($_POST) {
                     </thead>
                     <tbody>
                         <?php
-                        foreach ($_SESSION["array"] as $cliente) {
+                        foreach ($_SESSION["listadoClientes"] as $cliente) {
                             echo "<tr>";
                             echo "<td>" . $cliente["nombre"] . "</td>";
                             echo "<td>" . $cliente["dni"] . "</td>";
--- funcion_promediar.php
+++ funcion_promediar.php
@@ -1,24 +1,31 @@
 <?php
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+function promediar($aNumeros){
+    $suma = 0;
+    foreach($aNumeros as $numero){
+        $suma += $numero;
+    }
+    return $suma / count($aNumeros);
+}
 
-
-//Definición
-function promediar($aNumeros) {
-   foreach ($aArray as $item){
-       $promedio = 
-       $item ++ $item;
-   }
-   return $promedio;
+//Definicion
+function maximo($aNumeros)
+{
+    $maximo = 0;
+    foreach ($aNumeros as $numero) {
+        if ($numero > $maximo) {
+            $maximo = $numero;
+        }
+    }
+    return $maximo;
 }
 
 //Uso
-$aNotas = array (8, 4, 5, 3, 9, 1)
-echo "El promedio es " . promediar($aNotas). "<br>";
+$aNotas = array(8, 4, 5, 3, 9, 1);
+$aSueldo = array(800.30, 400.87, 500.45, 300, 900.70, 100, 900, 1800);
 
-?>
+echo "La nota máxima es: " . maximo($aNotas) . "<br>";
+echo "El sueldo máximo es: " . maximo($aSueldo);
 
 
 
