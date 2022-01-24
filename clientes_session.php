c6be014a738af852b751221dfebf72e363f6e4ba
leandroamar <leandro.mathias.amar@gmail.com>
2022-01-24 17:26:04 -0300

    cambios

--- clientes_session.php
+++ clientes_session.php
@@ -1,92 +1,97 @@
 <?php
 
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 session_start();
-
-if (!isset ($_SESSION["clientes"])){
-    $_SESSION["clientes"] = array();
+if (!isset($_SESSION["array"])) {
+    $_SESSION["array"] = array();
 }
 
+//session_destroy();
 
-if($_POST){
-    $_SESSION["clientes"][] = array (
-    "nombre" => $_REQUEST["txtNombre"],
-    "dni" => $_REQUEST["txtDni"],
-    "telefono" => $_REQUEST["txtTelefono"],
-    "edad" => $_REQUEST["txtEdad"],
-    );
+
+if ($_POST) {
+
+
+    if (isset($_POST["btnAgregar"])) {
+        $_SESSION["array"][] = array(
+            "nombre" => $_REQUEST["txtNombre"],
+            "dni"  => $_REQUEST["txtDni"],
+            "telefono"  => $_REQUEST["txtTelefono"],
+            "edad"  => $_REQUEST["txtEdad"]
+        );
+    }
+    if (isset($_POST["btnEliminar"])) {
+        
+        session_destroy();
+        $_SESSION["array"] = array();
+    }
 }
-
 ?>
 
 <!DOCTYPE html>
 <html lang="es">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Listado de Productos</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-            integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    </head>
-    <body>
-        <main class="container">
-            <div class="row my-5">
-                <div class="col-12 text-center">
-                    <h1>Tabla de Clientes</h1>
-                </div>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center py-5">
+                <h1> Tabla de clientes </h1>
             </div>
-            <div class="row">
-                <div class="col-4">
-                    <form action="" method="POST">
-                        <div class="my-3">
-                            <label for="">Nombre:
-                                <input type="text" name="txtNombre" id="txtNombre" class="form-control" placeholder="Ingrese el nombre y apellido">
-                            </label>
-                        </div>
-                        <div class="my-3">
-                            <label for="">DNI:
-                                <input type="text" name="txtDni" id="txtDni" class="form-control" placeholder="11111111">
-                            </label>
-                        </div>
-                        <div class="my-3">
-                            <label for="">Telefono:
-                                <input type="text" name="txtTelefono" id="txtTelefono" class="form-control" placeholder="1111111111">
-                            </label>
-                        </div>
-                        <div class="my-3">
-                            <label for="">Edad:
-                                <input type="text" name="txtEdad" id="txtEdad" class="form-control" placeholder="99">
-                            </label>
-                        </div>
-                        <div class="my-3">
-                            <button type="submit" class="btn btn-primary">Enviar</button>
-                            <button type="submit" class="btn btn-danger">Eliminar</button>
-                        </div>
-                    </form>
-                </div>
-                <div class="col-8">
-                    <table class="table table-hover border">
-                        <tr>
-                            <th>Nombre:</th>
-                            <th>DNI:</th>
-                            <th>Teléfono:</th>
-                            <th>Edad:</th>
-                        </tr>
-                        <?php foreach ($_SESSION["clientes"] as $cliente) { ?>
-                            <tr>
-                                <td><?php echo $cliente["nombre"]; ?></td>
-                                <td><?php echo $cliente["dni"]; ?></td>
-                                <td><?php echo $cliente["telefono"]; ?></td>
-                                <td><?php echo $cliente["edad"]; ?></td>
-                            </tr>
-                        <?php } ?>  
-                    </table>
-                </div>
+        </div>
+        <div class="row">
+            <div class="col-3 offset-1 me-5">
+                <form action="" method="POST" class="form">
+                    <label for="txtNombre"> Nombre:</label>
+                    <input type="text" name="txtNombre" class="form-control my-2 " placeholder="Ingrese el nombre y apellido">
+
+                    <label for="txtDni">DNI:</label>
+                    <input type="text" name="txtDni" class="form-control my-2" placeholder="11111111">
+
+                    <label for="txtTelefono">Telefono:</label>
+                    <input type="tel" name="txtTelefono" class="form-control my-2" placeholder="1111111111">
+
+                    <label for="txtEdad">Edad:</label>
+                    <input type="text" name="txtEdad" class="form-control my-2" placeholder="99">
+
+                    <input type="submit" name="btnAgregar" class="btn bg-primary text-white" value="Enviar">
+                    <input type="submit" name="btnEliminar" class="btn bg-danger text-white" value="eliminar">
+                </form>
             </div>
-        </main>
-    </body>
+            <div class="col-6 ms-5">
+                <table class="table table-hover shadow border">
+                    <thead>
+                        <th>Nombre:</th>
+                        <th>DNI:</th>
+                        <th>Telefono:</th>
+                        <th>Edad:</th>
+                    </thead>
+                    <tbody>
+                        <?php
+                        foreach ($_SESSION["array"] as $cliente) {
+                            echo "<tr>";
+                            echo "<td>" . $cliente["nombre"] . "</td>";
+                            echo "<td>" . $cliente["dni"] . "</td>";
+                            echo "<td>" . $cliente["telefono"] . "</td>";
+                            echo "<td>" . $cliente["edad"] . "</td>";
+                            echo "</tr>";
+                        }
+
+                        ?>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</body>
+
 </html>
